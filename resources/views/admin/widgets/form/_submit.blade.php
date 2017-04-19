60d0187d83cdb71ed370f840402c7dad2d065154
unlike777 <unlike777@gmail.com>
2017-04-19 09:43:13 +0300

    Разграничение логики сохранения и сохранения и закрытия формы

--- resources/views/admin/widgets/form/_submit.blade.php
+++ resources/views/admin/widgets/form/_submit.blade.php
@@ -2,7 +2,7 @@
     <div class="col-md-offset-3 col-md-9">
         <button type="submit" class="btn btn-default">Сохранить</button>
         &nbsp;&nbsp;
-        <button type="submit" class="btn btn-default">Применить</button>
+        <button type="submit" class="btn btn-default" name="save_and_close" value="1">Сохранить и закрыть</button>
         &nbsp;&nbsp;&nbsp;
         или
         &nbsp;&nbsp;&nbsp;
