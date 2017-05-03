2b7915ca7e4d7e872a8e2b3f86ac0b4ee18f1c09
unlike777 <unlike777@gmail.com>
2017-05-04 00:02:21 +0300

    Убираем сорсмапы

--- gulpfile.js
+++ gulpfile.js
@@ -21,33 +21,33 @@ gulp.task('fonts', function() {
 
 gulp.task('vendors:js', function() {
     return gulp.src(mainBowerFiles('**/*.js'), { base: 'resources/assets/admin/bower_components' })
-        .pipe(sourcemaps.init())
+        // .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(concat('vendor.js'))
-        .pipe(sourcemaps.write())
+        // .pipe(sourcemaps.write())
         .pipe(gulp.dest('public/assets/admin/vendor'));
 });
 
 gulp.task('vendors:css', function() {
     return gulp.src(mainBowerFiles('**/*.{css,less}'), { base: 'resources/assets/admin/bower_components' })
         .pipe(less())
-        .pipe(sourcemaps.init())
+        // .pipe(sourcemaps.init())
         .pipe(minifyCSS())
         .pipe(concat('vendor.css'))
-        .pipe(sourcemaps.write())
+        // .pipe(sourcemaps.write())
         .pipe(gulp.dest('public/assets/admin/vendor'));
 });
 
 gulp.task('js', function() {
     return gulp.src(['resources/assets/admin/libs/**/*.js', 'resources/assets/admin/js/**/*.js'])
         .pipe(plumber())
-        .pipe(sourcemaps.init())
+        // .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(uglify())
         .pipe(concat('app.js'))
-        .pipe(sourcemaps.write())
+        // .pipe(sourcemaps.write())
         .pipe(gulp.dest('public/assets/admin'));
 });
 
@@ -55,10 +55,10 @@ gulp.task('css', function() {
     return gulp.src(['resources/assets/admin/css/sb/*.less', 'resources/assets/admin/css/**/*.less'])
         .pipe(plumber())
         .pipe(less())
-        .pipe(sourcemaps.init())
+        // .pipe(sourcemaps.init())
         .pipe(minifyCSS())
         .pipe(concat('app.css'))
-        .pipe(sourcemaps.write())
+        // .pipe(sourcemaps.write())
         .pipe(gulp.dest('public/assets/admin'));
 });
 
